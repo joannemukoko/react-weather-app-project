986eeccef91428ea21b01a604f4e72fd50e96c9c
joannemukoko <97846226+joannemukoko@users.noreply.github.com>
2022-05-12 17:05:24 +0100

    Added footer with signature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by {""}{" "}
+        <a
+          href="https://www.linkedin.com/in/joannemukoko/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Joanne Mukoko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/joannemukoko/react-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
